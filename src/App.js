8912084567c035b8a243f58a16180227de9eb210
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-26 20:35:14 -0500

    Add header component and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     //BEM convention
     <div className="app">
-     <h1>Let's do FB-Clone</h1>
+     <Header />
 
-     {/* Header  */}
-
-     {/* App Body */}
+     <div className="app__body">
+        
         {/* Sidebar */}
         {/* Feed */}
         {/* Widgets */}
+
+     </div>
     </div>
   );
 }
