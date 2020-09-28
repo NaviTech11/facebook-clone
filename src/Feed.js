976ffa1d17672dcfba3f931e31bb64b9761fc16e
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-28 12:09:47 -0500

    Add Post component and Styles

--- src/Feed.js
+++ src/Feed.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
+import Post from './Post';
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
             <MessageSender />
+            
+            <Post 
+                profilePic="https://lh3.googleusercontent.com/ogw/ADGmqu-tMGiUa6ofUSuJRP761hkHy1Vp4BOatd7-SFSt=s32-c-mo"
+                message="Great stuff"
+                timestamp="Sept 27 2020"
+                username="Ivan H"
+                image="https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+            />
+            <Post 
+                profilePic="https://lh3.googleusercontent.com/ogw/ADGmqu-tMGiUa6ofUSuJRP761hkHy1Vp4BOatd7-SFSt=s32-c-mo"
+                message="Great stuff"
+                timestamp="Sept 27 2020"
+                username="Ivan H"
+               
+            />
+            <Post />
+            <Post />
         </div>
     )
 }
